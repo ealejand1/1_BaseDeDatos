c14955723af9da5eb35738b715aba0bff6ef98aa
Erik <erikalejjandro1112@gmail.com>
2023-02-19 21:30:24 +0100

    19_Febrero

--- 2ºEva/Cuestionarios/Cuestionario 32.sql
+++ 2ºEva/Cuestionarios/Cuestionario 32.sql
@@ -2,4 +2,20 @@
 
 select matricula from automoviles where locate(left(matricula,1),right(matricula,6)) > 0;
 
-select substring(matricula,5,length(matricula)) from automoviles; //matriculas letras
+select substring(matricula,5,length(matricula)) from automoviles; //matriculas letras;
+
+//
+
+select nombre,apellido1,concat(year(fechaNac),"-",dayname(fechaNac)) as diaNacimiento from personas where DNI in(select DNI from propietarios where matricula in (select matricula from polizas where (fechaBaja is not null or year(fechaAlta) < 2001) and nombreAseguradora in (select nombre from aseguradoras where telefono like "%00%")));
+
+select concat(left(nombre,1),".",apellido1," ",apellido2) as Nombre_y_apellidos from personas where ciudad like "Madrid" and DNI in (select DNI from propietarios group by DNI having count(*) > 17);
+
+//
+
+select nombre,apellido1,timestampdiff(YEAR,fechaNac,curdate()) - (select avg(timestampdiff(YEAR,fechaNac,curdate()))as media from personas) as Años_sobre_la_media from personas where (timestampdiff(YEAR,fechaNac,curdate()) - (select avg(timestampdiff(YEAR,fechaNac,curdate()))as media from personas)) > 0 and DNI in (select DNI from propietarios where matricula in (select matricula from automoviles where year(fechaPermCirc) = 2003 and (marca like "AUDI" or marca like "FORD")));
+
+//
+
+select year(fechaPermCirc) as año,max(month(fechaPermCirc)) as mes from automoviles where matricula in (select matricula from polizas where timestampdiff(month,fechaAlta,fechaBaja) > 25) group by año;
+
+
--- 2ºEva/Cuestionarios/Cuestionario 32_2.sql
+++
@@ -1,6 +0,0 @@
-select matricula,marca,modelo from automoviles where matricula in (select matricula from propietarios where substring(DNI,8,1) != 3) and ((substring(matricula,5,1) like "C" and right(matricula,1) like "B") and (substring(matricula,6,1) like "M" or substring(matricula,6,1) like "N")) or ((substring(matricula,5,1) like "B" and right(matricula,1) like "C") and (substring(matricula,6,1) like "M" or substring(matricula,6,1) like "N"));
-
-
-
-
-select matricula from propietarios where substring(DNI,8,1) = 3;
