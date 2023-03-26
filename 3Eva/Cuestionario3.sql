5f87885e9296d41b3d524b1926af27c4f51a0dbe
Erik <erikalejjandro1112@gmail.com>
2023-03-26 18:38:36 +0200

    26_Marzo

--- 3Eva/Cuestionario3.sql
+++ 3Eva/Cuestionario3.sql
@@ -8,3 +8,18 @@ select auto.matricula,mul.matricula from automoviles auto left join multas_a_veh
 select per.DNI,pol.matricula,pol.cuantia,abs(pol.cuantia - (select avg(cuantia) from polizas where nombreAseguradora like "Allianz")) as diferencia from personas per join propietarios pro join automoviles auto join polizas pol on per.DNI=pro.DNI and pro.matricula=auto.matricula and auto.matricula=pol.matricula and auto.marca like "Mercedes" and pol.nombreAseguradora like "Allianz";
 
 select per.nombre,per.apellido1,per.apellido2,count(*) as num  from personas per join propietarios pro join automoviles auto join multas_a_vehiculos mul on per.DNI = pro.DNI and pro.matricula=auto.matricula and auto.matricula = mul.matricula and mul.codSancion = 11 group by per.nombre,per.apellido1,per.apellido2 having num > 1;
+//
+
+select per.nombre as nombre,per.DNI,pro.matricula from personas per join propietarios pro join automoviles auto join polizas pol on per.DNI=pro.DNI and pro.matricula = auto.matricula and auto.matricula=pol.matricula where pol.fechaAlta in (select min(fechaAlta) from polizas);
+
+select per.ciudad,count(codSancion) as num from personas per join propietarios pro join automoviles auto join multas_a_vehiculos mul on per.DNI=pro.DNI and pro.matricula = auto.matricula and auto.matricula =mul.matricula and (mul.codSancion = 6 or mul.codSancion = 7 or mul.codSancion = 8 or mul.codSancion = 9 or mul.codSancion = 10 ) group by per.ciudad order by num desc limit 3;
+
+//
+
+select distinct per.nombre,per.apellido1,per.apellido2 from personas per join propietarios pro join automoviles auto on per.DNI=pro.DNI and pro.matricula=auto.matricula where ciudad like "Lerida" and auto.matricula in (select matricula from multas_a_vehiculos) and auto.matricula not in (select matricula from polizas);
+
+select ase.nombre,count(po.matricula) as num from aseguradoras ase join polizas po join automoviles auto join modelos mo on ase.nombre=po.nombreAseguradora and po.matricula = auto.matricula and auto.marca=mo.marca and auto.modelo=mo.modelo where auto.marca like "Mercedes" and mo.gama like "Alta" group by ase.nombre having num > 1 order by ase.nombre;
+
+
+
+
--- 3Eva/Cuestionario4.sql
+++ 3Eva/Cuestionario4.sql
@@ -1,6 +1,18 @@
-select de.dept_name,concat(left(emp.first_name,3),".",right(emp.last_name,3)) as ext_empleado,sal.salary,max(sal.to_date) from departments de join dept_manager man join employees emp join salaries sal on de.dept_no = man.dept_no and man.emp_no = emp.emp_no and emp.emp_no = sal.emp_no and sal.salary > 100000 and group by ext_empleado,de.dept_name,sal.salary;
+ select de.dept_name,concat(left(emp.first_name,3),".",right(emp.last_name,3)) as ext_empleado,sal.salary,max(sal.to_date) from departments de join dept_manager man join employees emp join salaries sal on de.dept_no = man.dept_no and man.emp_no = emp.emp_no and emp.emp_no = sal.emp_no and sal.salary > 100000 and group by ext_empleado,de.dept_name,sal.salary;
 //
 
 select concat(emp.first_name," ",emp.last_name) as empleado,ti.title,sal.salary from employees emp join titles ti join salaries sal on emp.emp_no = ti.emp_no and emp.emp_no=sal.emp_no and emp.hire_date = sal.from_date and ti.title like "Senior Staff" order by sal.salary desc;
 
+//
+
+select depar.dept_name,concat(left(emp.first_name,3),".",right(emp.last_name,3)) as ext_empleado,sal.salary from departments depar join dept_manager de join employees emp join salaries sal on depar.dept_no=de.dept_no and de.emp_no=emp.emp_no and emp.emp_no =sal.emp_no where sal.to_date like "9999%" and sal.salary > 100000;
+
+select concat(left(emp.first_name,1),".",left(emp.last_name,3)) as cod_emp,ti.title,ti.from_date,ti.to_date from employees emp join titles ti on emp.emp_no=ti.emp_no where ti.title like "Manager" and ti.to_date like "9999%";
+
+select concat(left(emp.first_name,1),".",left(emp.last_name,3)) as cod_emp where ti.to_date like "9999%" and ti.title like "Manager" or ti.title not like "Manager";
+
+//
+select concat(emp.first_name," ",emp.last_name) as emp,sal.salary from employees emp join salaries sal join dept_emp dept on emp.emp_no=sal.emp_no and emp.emp_no=dept.emp_no and ((year(dept.from_date) = year(sal.from_date)) and year(dept.from_date) > 2000) and sal.salary > 90000 and day(dept.from_date)= 31;
+
+select concat(emp.first_name," ",emp.last_name) as emp,ti.title,sal.salary from employees emp join titles ti join salaries sal on emp.emp_no = sal.emp_no and emp.emp_no = ti.emp_no and emp.hire_date = sal.from_date where ti.title like "%Senior%" order by sal.salary desc limit 10;
 
